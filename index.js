bfcea9eefa760f929138ce443a9b9e8eee7a4df8
Omar <Omar>
2022-01-22 08:14:08 -0500

    added cors

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGO_URI;
+const cors = require("cors");
 
 mongoose.connect(MONGO_URI);
 
 app.use(express.json());
 app.use("/books", require("./controllers/books"));
+app.use(cors());
+
+app.get("/books/:id", function (req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
 
 app.get("/", (req, res) => {
   res.send("GET /books");
